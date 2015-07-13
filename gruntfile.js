07881d60b60430518b1ad11a5ba9629545910b0c
JJ Schaus <jschaus@renegadecommunications.com>
2015-07-13 19:17:25 -0400

    init commit

---
+++ gruntfile.js
@@ -0,0 +1,101 @@
+module.exports = function(grunt) {
+  // var BUILD_DIR_CSS = 'deploy/assets/css';
+  // var SRC_DIR_CSS = 'src/scss';
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("mm-dd-yyyy") %> - <%= grunt.template.date("h:MM:ss TT") %> */\n'
+      },
+      dist: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'deploy/assets/css/main.css' : 'src/scss/main.scss'
+        }
+      },
+      dev: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'deploy/assets/css/main.css' : 'src/scss/main.scss'
+        }
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      dist: {
+        files: {
+          'deploy/assets/js/script.min.js' : 'src/js/*.js'
+        }
+      },
+      dev: {
+        options: {
+          compress: false,
+          beautify: true,
+          mangle: false
+        },
+        files: {
+          'deploy/assets/js/script.min.js' : 'src/js/*.js'
+        }
+      }
+    },
+
+    includes: {
+      deploy: {
+        cwd: 'src/pages',
+        src: ['*.html'],
+        dest: './deploy/',
+          options: {
+            includePath: 'src/partials'
+        }
+      } //deploy  
+    }, //includes
+     
+    imagemin:{
+     dynamic: {                         // Another target 
+          files: [{
+            expand: true,                  // Enable dynamic expansion 
+            cwd: 'src/imgs/',                   // Src matches are relative to this path 
+            src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match 
+            dest: 'deploy/assets/imgs/'                  // Destination path prefix 
+          }]
+        }
+    },
+    watch: {
+      // compass: {
+      //   files: ['src/**/*.scss'],
+      //  tasks: ['compass:dev']
+      // },
+      css: {
+        files: ['src/**/*.scss'],
+        tasks: ['sass:dev']
+        },
+      scripts: {
+        files: ['src/**/*.js'],
+        tasks: ['uglify']
+      },
+      html: {
+        files: ['src/**/*.html'],
+        tasks: ['includes:deploy']
+      }
+    } // watch 
+  }); // grunt.initConfig
+
+  // grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-includes');   
+  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('deploy', ['uglify:dist', 'sass:dist','imagemin']);
+
+};
