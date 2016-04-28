9cbe91112dd3460c1429f362164fbd5c4d762a80
jj <jjschausweb@gmail.com>
2016-04-28 16:05:24 -0400

    updated packages

--- gruntfile.js
+++ gruntfile.js
@@ -111,7 +111,7 @@ module.exports = function(grunt) {
   }); // grunt.initConfig
 
   // grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   //grunt.loadNpmTasks('grunt-contrib-imagemin');
