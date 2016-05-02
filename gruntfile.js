f233b3ea7e29ec2cd7e43e37ec4a54b638775642
jj <jjschausweb@gmail.com>
2016-05-02 16:44:14 -0400

    Rolled back to node js 4.4.3

--- gruntfile.js
+++ gruntfile.js
@@ -1,14 +1,14 @@
 module.exports = function(grunt) {
-  // var BUILD_DIR_CSS = 'deploy/assets/css';
-  // var SRC_DIR_CSS = 'src/scss';
+    // var BUILD_DIR_CSS = 'deploy/assets/css';
+    // var SRC_DIR_CSS = 'src/scss';
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-    banner: '/* <%= pkg.name %> - version <%= pkg.version %>\n' +
-          ' * <%= grunt.template.today("mm-dd-yyyy") %>\n' +
-          ' * <%= grunt.template.date("h:MM:ss TT") %>\n' +
-          ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n */\n',
+        banner: '/* <%= pkg.name %> - version <%= pkg.version %>\n' +
+            ' * <%= grunt.template.today("mm-dd-yyyy") %>\n' +
+            ' * <%= grunt.template.date("h:MM:ss TT") %>\n' +
+            ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n */\n',
         usebanner: {
             dist: {
                 options: {
@@ -16,120 +16,120 @@ module.exports = function(grunt) {
                     banner: '<%= banner %>'
                 },
                 files: {
-                    src: [ 'deploy/assets/css/main.css', 'deploy/assets/main-min.css']
+                    src: ['deploy/assets/css/main.css', 'deploy/assets/main-min.css']
                 }
             }
         },
-    sass: {
-      options: {
-      require: 'susy'
-      },
-      dist: {
-        options: {
-          style: 'compressed'
+        sass: {
+            options: {
+                require: 'susy'
+            },
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'deploy/assets/css/main.css': 'src/scss/main.scss'
+                }
+            },
+            dev: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    'deploy/assets/css/main.css': 'src/scss/main.scss'
+                }
+            }
         },
-        files: {
-          'deploy/assets/css/main.css' : 'src/scss/main.scss'
-        }
-      },
-      dev: {
-        options: {
-          style: 'expanded'
+
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n\n'
+            },
+            dist: {
+                files: {
+                    'deploy/assets/js/script.min.js': 'src/js/*.js'
+                }
+            },
+            dev: {
+                options: {
+                    compress: false,
+                    beautify: true,
+                    mangle: false
+                },
+                files: {
+                    'deploy/assets/js/script.min.js': 'src/js/*.js'
+                }
+            }
         },
-        files: {
-          'deploy/assets/css/main.css' : 'src/scss/main.scss'
-        }
-      }
-    },
 
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n\n'
-      },
-      dist: {
-        files: {
-          'deploy/assets/js/script.min.js' : 'src/js/*.js'
-        }
-      },
-      dev: {
-        options: {
-          compress: false,
-          beautify: true,
-          mangle: false
+        includes: {
+            deploy: {
+                cwd: 'src/pages',
+                src: ['*.html', '*.php'],
+                dest: './deploy/',
+                options: {
+                    includePath: 'src/partials'
+                }
+            } //deploy  
+        }, //includes
+
+        // imagemin:{
+        //  dynamic: {                         // Another target 
+        //       files: [{
+        //         expand: true,                  // Enable dynamic expansion 
+        //         cwd: 'src/imgs/',                   // Src matches are relative to this path 
+        //         src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match 
+        //         dest: 'deploy/assets/imgs/'                  // Destination path prefix 
+        //       }]
+        //     }
+        // },
+        cachebreaker: {
+            dev: {
+                options: {
+                    match: ['main.css'],
+                    src: {
+                        path: 'deploy/assets/css/main.css'
+                    }
+                },
+                files: {
+                    src: ['src/partials/header.html']
+                }
+            }
         },
-        files: {
-          'deploy/assets/js/script.min.js' : 'src/js/*.js'
-        }
-      }
-    },
+        watch: {
+            // compass: { 
+            //   files: ['src/**/*.scss'],
+            //  tasks: ['compass:dev']
+            // },
+            options: {
+                livereload: true
+            },
+            css: {
+                files: ['src/**/*.scss'],
+                tasks: ['sass:dev', 'usebanner', 'cachebreaker']
+            },
+            scripts: {
+                files: ['src/**/*.js'],
+                tasks: ['uglify']
+            },
+            html: {
+                files: ['src/**/*.html', 'src/**/*.php'],
+                tasks: ['includes:deploy']
+            }
+        } // watch 
+    }); // grunt.initConfig
 
-    includes: {
-      deploy: {
-        cwd: 'src/pages',
-        src: ['*.html','*.php'],
-        dest: './deploy/',
-          options: {
-            includePath: 'src/partials'
-        }
-      } //deploy  
-    }, //includes
-     
-    // imagemin:{
-    //  dynamic: {                         // Another target 
-    //       files: [{
-    //         expand: true,                  // Enable dynamic expansion 
-    //         cwd: 'src/imgs/',                   // Src matches are relative to this path 
-    //         src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match 
-    //         dest: 'deploy/assets/imgs/'                  // Destination path prefix 
-    //       }]
-    //     }
-    // },
-             cachebreaker: {
-              dev: {
-                  options: {
-            match: [ 'main.css'],
-                      src: {
-                          path: 'deploy/assets/css/main.css'
-                      }
-                  },
-                  files: {
-                      src: ['src/partials/header.html']
-                  }
-              }
-          },
-    watch: {
-      // compass: { 
-      //   files: ['src/**/*.scss'],
-      //  tasks: ['compass:dev']
-      // },
-      options:{
-        livereload: true
-      },  
-      css: {
-        files: ['src/**/*.scss'],
-        tasks: ['sass:dev','usebanner','cachebreaker']
-        },
-      scripts: {
-        files: ['src/**/*.js'],
-        tasks: ['uglify']
-      },
-      html: {
-        files: ['src/**/*.html','src/**/*.php' ],
-        tasks: ['includes:deploy']
-      }
-    } // watch 
-  }); // grunt.initConfig
+    // grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    //grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-includes');
+    grunt.loadNpmTasks('grunt-banner');
+    grunt.loadNpmTasks('grunt-cache-breaker');
 
-  // grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  //grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-includes');   
-  grunt.loadNpmTasks('grunt-banner');
-grunt.loadNpmTasks('grunt-cache-breaker');
-
-  grunt.registerTask('default', ['watch']);
-  grunt.registerTask('deploy', ['uglify:dist','sass:dist','usebanner']);
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('deploy', ['uglify:dist', 'sass:dist', 'usebanner']);
 
 };
